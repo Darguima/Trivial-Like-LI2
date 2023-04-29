aa722f299524fa301c0312c8199fc737c3976669
HugoLRauber <hugoleite8882@gmail.com>
2023-04-29 19:02:01 +0100

    adicionados structs de mobs, existe um conflito no struct mob sem resolver

--- src/state.h
+++ src/state.h
@@ -10,12 +10,62 @@ typedef enum scene { /* São as diferentes páginas que podem aparecer ao longo 
 	Sair
 } Scene;
 
-typedef enum arma { /* Diferentes armas que o player pode ter */
-	Nada,
-	Espada,
+typedef enum tipoArma { /* Diferentes armas que o player pode ter */
+	Punhos,
+	Garras,
+	EspadaOxidada,
+	EspadaLonga,
 	Arco,
+	Acido,
+	Cetro,
 	/* Adicionar mais no futuro */
+} TipoArma;
+
+typedef enum distanciaArma { // Este struc tem as possíveis distancias nas quais as armas funcionam
+	Melee,
+	Curta,
+	Media,
+	Longa,
+	MuitoLonga,
+} DistanciaArma;
+
+typedef struct arma {
+    TipoArma tipoarma; // recebe um tipo de arma
+	DistanciaArma distanciaarma;
+    int dano; // dano da arma
+    int probA; // isto é a probabilidade de o ataque acertar no objetivo, de 0 a 100
+	char* mensagem; // texto que a arma mostra quando se dá inspect no inventário (feature que pode ser implementada ou não)
 } Arma;
+
+
+
+/* // o segunite é deixado caso seja preciso
+
+Arma punhos = {Melee, 7, 50, "Um par de punhos que, nas mãos adequadas, fazem grandes estragos "};
+
+Arma garras = {Curta, 25, 60, "Garras ensanguentadas que saem das mãos de um grotesco monstro. Urghh..."};
+
+Arma espadaOxidada = {Media,  35, 75, "Espada que pertenceu a um soldado caído em combate."};
+
+Arma espadaLonga = {Media, 30, 95, "Espada forjada pelos melhores ferreiros. Embora não seja muito potente, raramente falha ao seu portador."};
+
+Arma arco = {MuitoLonga, 50, 25, "Um arco bastante flexível que causa muito dano mas possui uma terrível precisão."};
+
+Arma acido = {Longa, 45, 20, "Um líquido lançado por certos monstros que desfaz tudo o que toca."};
+
+Arma cetro = {Longa, 15, 100, "Um cetro carregado de magia. O seu baixo dano é compensado com a precisão dos seus projéteis."};*/
+
+Arma armas[] = {
+	{Punhos ,Melee, 7, 50, "Um par de punhos que, nas mãos adequadas, fazem grandes estragos "}, // 0
+	{Garras, Curta, 20, 60, "Garras ensanguentadas que saem das mãos de um grotesco monstro. Urghh..."}, // 1
+	{EspadaOxidada, Media,  25, 75, "Espada que pertenceu a um soldado caído em combate."}, // 2
+	{EspadaLonga, Media, 30, 95, "Espada forjada pelos melhores ferreiros. Embora não seja muito potente, raramente falha ao seu portador."}, // 3
+	{Arco, MuitoLonga, 45, 30, "Um arco bastante flexível que causa muito dano mas possui uma terrível precisão."}, // 4
+	{Acido, Longa, 40, 15, "Um líquido lançado por certos monstros que desfaz tudo o que toca."}, // 5
+	{Cetro, Longa, 15, 100, "Um cetro carregado de magia. O seu baixo dano é compensado com a precisão dos seus projéteis."} // 6
+};
+
+
 
 typedef struct coordenadas {
 	int x, y;
@@ -33,9 +83,48 @@ typedef struct statusJogador {
 	Arma* inventario;
 } StatusJogador;
 
+
+typedef enum tipoMob { // o tipo do mob a chamar
+	Esqueleto, /* E */
+	SoldadoEsqueleto, /* S */
+	Vampiro, /* V */ 
+	Mutante, /* M */
+	Aranha,  /* A */
+	Zombie,  /* Z */
+	/* Possibilidade de adicionar mais no futuro */
+} TipoMob;
+
+typedef struct mob { // podem haver mais que um mob com armas diferentes
+	// TipoMob tipomob; // recebe qual o tipo do mob
+	Arma arma; // o mob possui uma arma só. Aqui temos de aplicar uma das armas do struct Armas
+	int vida; 
+	int raioVisao;  // o raio de visão pode variar entre 1 e 10 (provisório). o raio de visão mede-se em quantas 'casas' o mob consegue ver o jogador e começar a atacar 
+} Mob;
+
+Mob esqueleto1 = {armas[2], 50, 3};
+Mob esqueleto2 = {armas[0], 50, 3};
+
+Mob SoldadoEsqueleto1 = {armas[3], 70, 4};
+
+Mob vampiro1 = {armas[1], 150, 8};
+Mob vampiro1 = {armas[6], 60, 10};
+
+Mob mutante1 = {armas[0], 40, 2};
+Mob mutante2 = {armas[0], 20, 2};
+
+Mob aranha1 = {armas[5], 40, 10 };
+Mob aranha1 = {armas[5], 60, 8 };
+
+Mob zombie1 = {armas[1], 100, 2 };
+
+typedef struct statusMobs {
+	Coordenadas posicao;
+	Mob mob; // qual o mob e as suas características
+} StatusMobs;
+
 typedef enum elementosDoMapa {
 	Jogador, /* @ */
-	Monstros, /* M - com foreground ou background vermelho */
+	Mobs, /* depende do tipo do mob - com foreground ou background vermelho */
 	NPC, /* & */
 	Paredes, /* # */
 	PortaNormal, /* + - serve para fechar as salas, no futuro se for possível implementar o conceito de chaves escondidas */
