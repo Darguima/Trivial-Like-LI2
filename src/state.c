a2b66c559f1fc8d2a583b5fe29725bb53388c59a
Darguima <dsgdevbraga@gmail.com>
2023-05-05 13:38:28 +0100

    structs

--- src/state.c
+++ src/state.c
@@ -1,6 +1,29 @@
 #include <stdlib.h>
 #include "state.h"
 
+const Arma punho = {Punhos, 7, 50, "Um par de punhos que, nas mãos adequadas, fazem grandes estragos "};
+const Arma garras = {Garras, 20, 60, "Garras ensanguentadas que saem das mãos de um grotesco monstro. Urghh..."};
+const Arma espadaOxidada = {EspadaOxidada, 25, 75, "Espada que pertenceu a um soldado caído em combate."};
+const Arma espadaLonga = {EspadaLonga, 30, 95, "Espada forjada pelos melhores ferreiros. Embora não seja muito potente, raramente falha ao seu portador."};
+const Arma arco = {Arco, 45, 30, "Um arco bastante flexível que causa muito dano mas possui uma terrível precisão."};
+const Arma acido = {Acido, 40, 15, "Um líquido lançado por certos monstros que desfaz tudo o que toca."};
+const Arma cetro = {Cetro, 15, 100, "Um cetro carregado de magia. O seu baixo dano é compensado com a precisão dos seus projéteis."};
+
+const Arma catalogoArmas[] = {punho, garras, espadaOxidada, espadaLonga, arco, acido, cetro};
+
+const Mob esqueleto1 = {espadaOxidada, 50, 3};
+const Mob esqueleto2 = {punho, 50, 3};
+const Mob SoldadoEsqueleto1 = {espadaLonga, 70, 4};
+const Mob vampiro1 = {garras, 150, 8};
+const Mob vampiro2 = {cetro, 60, 10};
+const Mob mutante1 = {punho, 40, 2};
+const Mob mutante2 = {punho, 20, 2};
+const Mob aranha1 = {acido, 40, 10};
+const Mob aranha2 = {acido, 60, 8};
+const Mob zombie1 = {garras, 100, 2};
+
+const Mob catalogoMobs[] = {esqueleto1, esqueleto2, SoldadoEsqueleto1, vampiro1, vampiro2, mutante1, mutante2, aranha1, aranha2, zombie1};
+
 State criarEstado(int colunas, int linhas)
 {
 	State state;
@@ -16,6 +39,13 @@ State criarEstado(int colunas, int linhas)
 	// Fill the matrix with values
 
 	state.sceneAtual = MenuInicial;
+
+	state.controloMenu.highlight = 1;
+	state.controloMenu.side = 0;
+	state.controloMenu.help = 0;
+
+	// state.armamento = 
+
 	state.jogoAtual.jogador.vida = 100;
 	state.jogoAtual.jogador.username = NULL;
 	state.jogoAtual.jogador.posicao.x = 3;
@@ -24,18 +54,12 @@ State criarEstado(int colunas, int linhas)
 	state.jogoAtual.jogador.mensagem = NULL;
 	state.jogoAtual.jogador.inventario = NULL;
 	state.jogoAtual.jogador.dinheiro = 0;
-	state.jogoAtual.jogador.armaPrincipal = Nada;
-	state.jogoAtual.jogador.armaSecundaria = Nada;
-	state.jogoAtual.Mapa = NULL;
-	
+	state.jogoAtual.jogador.armaPrincipal = punho;
+	state.jogoAtual.jogador.armaSecundaria = punho;
+
 	state.mapa.width = colunas;
 	state.mapa.height = linhas;
 	state.mapa.matrix = matrix;
 
-	state.jogoAtual.Mapa  = NULL;
-	state.controloMenu.highlight = 1;
-	state.controloMenu.side=0;
-	state.controloMenu.help=0;
-	
-  return state;
+	return state;
 }
--- src/state.h
+++ src/state.h
@@ -8,10 +8,11 @@ typedef enum scene
 	Jogo,							 /* O jogo em si, com o status do jogador, mapa e inventário */
 	Controlos,
 	Sobre,
-	Sair
+	Sair,
 } Scene;
 
-typedef enum tipoArma { /* Diferentes armas que o player pode ter */
+typedef enum armamento /* Diferentes armas que o player e os mobs pode ter */
+{
 	Punhos,
 	Garras,
 	EspadaOxidada,
@@ -19,26 +20,15 @@ typedef enum tipoArma { /* Diferentes armas que o player pode ter */
 	Arco,
 	Acido,
 	Cetro,
-	/* Adicionar mais no futuro */
-} TipoArma;
+} Armamento;
 
-typedef struct arma {
-    TipoArma tipoarma; // recebe um tipo de arma
-    int dano; // dano da arma
-    int probA; // isto é a probabilidade de o ataque acertar no objetivo, de 0 a 100
-	char* mensagem; // texto que a arma mostra quando se dá inspect no inventário (feature que pode ser implementada ou não)
+typedef struct arma
+{
+	Armamento tipoArma;
+	int dano;				// dano da arma
+	int probA;			// isto é a probabilidade de o ataque acertar no objetivo, de 0 a 100
+	char *mensagem; // texto que a arma mostra quando se dá inspect no inventário (feature que pode ser implementada ou não)
 } Arma;
-
-Arma armas[] = {
-	{Punhos , 7, 50, "Um par de punhos que, nas mãos adequadas, fazem grandes estragos "}, // 0
-	{Garras, 20, 60, "Garras ensanguentadas que saem das mãos de um grotesco monstro. Urghh..."}, // 1
-	{EspadaOxidada,  25, 75, "Espada que pertenceu a um soldado caído em combate."}, // 2
-	{EspadaLonga, 30, 95, "Espada forjada pelos melhores ferreiros. Embora não seja muito potente, raramente falha ao seu portador."}, // 3
-	{Arco, 45, 30, "Um arco bastante flexível que causa muito dano mas possui uma terrível precisão."}, // 4
-	{Acido, 40, 15, "Um líquido lançado por certos monstros que desfaz tudo o que toca."}, // 5
-	{Cetro, 15, 100, "Um cetro carregado de magia. O seu baixo dano é compensado com a precisão dos seus projéteis."} // 6
-};
-
 
 typedef struct coordenadas
 {
@@ -58,48 +48,33 @@ typedef struct statusJogador
 	Arma *inventario;
 } StatusJogador;
 
-typedef enum tipoMob { // o tipo do mob a chamar
-	Esqueleto, /* E */
+typedef enum tipoMob
+{										// o tipo do mob a chamar
+	Esqueleto,				/* E */
 	SoldadoEsqueleto, /* S */
-	Vampiro, /* V */ 
-	Mutante, /* M */
-	Aranha,  /* A */
-	Zombie,  /* Z */
-	/* Possibilidade de adicionar mais no futuro */
+	Vampiro,					/* V */
+	Mutante,					/* M */
+	Aranha,						/* A */
+	Zombie,						/* Z */
 } TipoMob;
 
-typedef struct mob { // podem haver mais que um mob com armas diferentes
-	// TipoMob tipomob; // recebe qual o tipo do mob
-	Arma arma; // o mob possui uma arma só. Aqui temos de aplicar uma das armas do struct Armas
-	int vida; 
-	int raioVisao;  // o raio de visão pode variar entre 1 e 10 (provisório). o raio de visão mede-se em quantas 'casas' o mob consegue ver o jogador e começar a atacar 
+typedef struct mob
+{
+	Arma arma;
+	int vida;
+	int raioVisao; // o raio de visão pode variar entre 1 e 10 (provisório). o raio de visão mede-se em quantas 'casas' o mob consegue ver o jogador e começar a atacar
 } Mob;
 
-Mob esqueleto1 = {armas[2], 50, 3};
-Mob esqueleto2 = {armas[0], 50, 3};
-
-Mob SoldadoEsqueleto1 = {armas[3], 70, 4};
-
-Mob vampiro1 = {armas[1], 150, 8};
-Mob vampiro1 = {armas[6], 60, 10};
-
-Mob mutante1 = {armas[0], 40, 2};
-Mob mutante2 = {armas[0], 20, 2};
-
-Mob aranha1 = {armas[5], 40, 10 };
-Mob aranha1 = {armas[5], 60, 8 };
-
-Mob zombie1 = {armas[1], 100, 2 };
-
-typedef struct statusMobs {
+typedef struct statusMobs
+{
 	Coordenadas posicao;
-	Mob mob; // qual o mob e as suas características
+	Mob mob;
 } StatusMobs;
 
 typedef enum elementosDoMapa
 {
 	Jogador,					/* @ */
-	Mobs,					/* M - com foreground ou background vermelho */
+	Mobs,							/* M - com foreground ou background vermelho */
 	NPC,							/* & */
 	Paredes,					/* # */
 	PortaNormal,			/* + - serve para fechar as salas, no futuro se for possível implementar o conceito de chaves escondidas */
@@ -116,7 +91,6 @@ typedef struct controlosMenu
 typedef struct jogoAtual
 {
 	StatusJogador jogador;
-	ElementosDoMapa *Mapa;
 } JogoAtual;
 
 typedef struct mapa
@@ -129,9 +103,12 @@ typedef struct mapa
 typedef struct state
 {
 	Scene sceneAtual;
-	JogoAtual jogoAtual;
+	
 	ControlosMenu controloMenu;
+	
+	JogoAtual jogoAtual;
 	Mapa mapa;
+	
 } State;
 
 State criarEstado(int colunas, int linhas);
