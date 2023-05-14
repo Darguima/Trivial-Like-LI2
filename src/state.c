66a13394e0ad39fbd1c046b570eccaa5388a7bc2
HugoLRauber <hugoleite8882@gmail.com>
2023-05-15 00:21:54 +0100

    arranjado o warning

--- src/state.c
+++ src/state.c
@@ -98,7 +98,7 @@ State criarEstado(int colunas, int linhas)
 	state.jogoAtual.objetos = NULL; // Alocado depois
 	state.jogoAtual.mensagem_descricao = "Encontra a porta para passar de mapa!";
 	state.jogoAtual.mensagem_controlos = "Utiliza as setas para te movimentares.";
-	state.jogoAtual.mensagem_direita = ' ';
+	state.jogoAtual.mensagem_direita = " ";
 
 	return state;
 }
