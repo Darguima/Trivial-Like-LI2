6f1e0f1d050f928b96cd64afe0bbb42cfb29b837
Darguima <dsgdevbraga@gmail.com>
2023-05-18 13:14:43 +0100

    + Colors

--- src/colors.c
+++ src/colors.c
@@ -17,11 +17,11 @@ void start_state_colors()
   init_color(FG_Player, 0, 0, 1000);
   init_color(FG_Moeda, 1000, 1000, 0);
   init_color(FG_Arma, 0, 1000, 0);
-  init_color(FG_Objeto, 500, 1000, 1000);
-  init_color(FG_Mob, 1000, 500, 500);
+  init_color(FG_Objeto, 500, 500, 1000);
+  init_color(FG_Mob, 1000, 0, 0);
   
-  init_color(BG_Parede, 660, 168, 168);
-  init_color(BG_ParedeNaoVisivel, 330, 84, 84);
+  init_color(BG_Parede, 1000, 1000, 1000);
+  init_color(BG_ParedeNaoVisivel, 500, 500, 500);
 
   init_pair(WhiteBlack, COLOR_WHITE, COLOR_BLACK);
   init_pair(YellowBlack, COLOR_YELLOW, COLOR_BLACK);
