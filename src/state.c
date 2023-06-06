4120351530ccd682c5e16a2a1b5dd54df4059e80
afooonso <afonsopedreira14@gmail.com>
2023-06-06 16:41:49 +0100

    Corrigida probabilidade dos mobs

--- src/state.c
+++ src/state.c
@@ -5,15 +5,15 @@
 #include "SalvarJogo/salvarJogo.h"
 
 Arma const punhos = {0, Punhos, "Punhos", 15, 50, "Um par de punhos que, nas maos adequadas, fazem grandes estragos "};
-Arma const garras = {1, Garras, "Garras", 10, 60, "Garras ensanguentadas que saem das mãos de um grotesco monstro. Urghh..."};
+Arma const garras = {1, Garras, "Garras", 10, 30, "Garras ensanguentadas que saem das maos de um grotesco monstro. Urghh..."};
 Arma const espadaOxidada = {2, EspadaOxidada, "Espada Oxidada", 20, 75, "Espada que pertenceu a um soldado caido em combate."};
 Arma const espadaLonga = {3, EspadaLonga, "Espada Longa", 25, 95, "Espada forjada pelos melhores ferreiros. Embora não seja muito potente, raramente falha ao seu portador."};
 Arma const arco = {4, Arco, "Arco", 40, 30, "Um arco bastante flexivel que causa muito dano mas possui uma terrivel precisao."};
 Arma const acido = {5, Acido, "Acido", 20, 25, "Um liquido lançado por certos monstros que desfaz tudo o que toca."};
 Arma const cetro = {6, Cetro, "Cetro", 15, 100, "Um cetro carregado de magia. O seu baixo dano e compensado com a precisão dos seus projeteis."};
-Arma const aniquiladora = {7, Aniquiladora, "Aniquiladora", 100, 15, "Desconhece-se a origem deste artefacto demoniaco mas sabe-se que o seu poder de destruicao nao tem limites."};
-Arma const foice = {8, Foice, "Foice", 300, 5, "Arma que a propria Morte possui. Se a sua lamina toca em alguem, morre instantaneamente."};
-Arma const computador = {9, Computador, "Computador", 1, 100, "Um mero computador... Não serve para muito nestas condicoes."};
+Arma const aniquiladora = {7, Aniquiladora, "Aniquiladora", 80, 7, "Desconhece-se a origem deste artefacto demoniaco mas sabe-se que o seu poder de destruicao nao tem limites."};
+Arma const foice = {8, Foice, "Foice", 120, 3, "Arma que a propria Morte possui. Se a sua lamina toca em alguem, morre instantaneamente."};
+Arma const computador = {9, Computador, "Computador", 1, 1, "Um mero computador... Não serve para muito nestas condicoes."};
 
 int const catalogoArmasLength = 10;
 Arma const catalogoArmas[] = {punhos, garras, espadaOxidada, espadaLonga, arco, acido, cetro, aniquiladora, foice, computador};
@@ -21,15 +21,15 @@ Arma const catalogoArmas[] = {punhos, garras, espadaOxidada, espadaLonga, arco, 
 Mob const esqueleto1 = {Esqueleto, "Esqueleto", 'E', espadaOxidada, 40, 40, 3};
 Mob const esqueleto2 = {Esqueleto, "Esqueleto", 'E', punhos, 40, 40, 3};
 Mob const soldadoEsqueleto1 = {SoldadoEsqueleto, "Soldado Esqueleto", 'S', espadaLonga, 60, 60, 4};
-Mob const vampiro1 = {Vampiro, "Vampiro", 'V', garras, 70, 70, 8};
-Mob const vampiro2 = {Vampiro, "Vampiro", 'V', cetro, 50, 50, 10};
+Mob const vampiro1 = {Vampiro, "Vampiro", 'V', garras, 70, 70, 6};
+Mob const vampiro2 = {Vampiro, "Vampiro", 'V', cetro, 50, 50, 2};
 Mob const mutante1 = {Mutante, "Mutante", 'M', punhos, 30, 30, 2};
 Mob const mutante2 = {Mutante, "Mutante", 'M', punhos, 10, 10, 2};
 Mob const aranha1 = {Aranha, "Aranha", 'A', acido, 30, 30, 10};
-Mob const aranha2 = {Aranha, "Aranha", 'A', acido, 40, 40, 8};
+Mob const aranha2 = {Aranha, "Aranha", 'A', acido, 20, 20, 8};
 Mob const zombie1 = {Zombie, "Zombie", 'Z', garras, 70, 70, 2};
-Mob const demonio = {Demonio, "Demonio", 'D', aniquiladora, 150, 150, 5};
-Mob const morte = {Morte, "Morte", 'W', foice, 200, 200, 10};
+Mob const demonio = {Demonio, "Demonio", 'D', aniquiladora, 120, 120, 3};
+Mob const morte = {Morte, "Morte", 'W', foice, 170, 170, 5};
 Mob const informatico = {Informatico, "Informatico", 'I', computador, 1000, 1000, 1};
 
 int const catalogoMobsLength = 13;
